502b2cca56e837b5d32ca47b7b39f5dce28a9851
Jacob McAllister <mcallisterj@westerntc.edu>
2024-06-03 12:01:09 -0500

    End of class 6-3-2024
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- 04_loading_assets/main.js
+++ 04_loading_assets/main.js
@@ -1,14 +1,32 @@
 const SPACE_BAR = 32;
 
 let player;
+let birdImage = null;
+let bg = null;
+
+
+/**
+ * Timing: The preload() function is called before the setup() function and
+ * ensures that all assets are fully loaded before the rest of the sketch begins to execute.
+ *
+ * Execution: It is executed only once, similar to setup().
+ *
+ * Blocking Behavior: During the execution of preload(), the sketch will pause,
+ * preventing the setup() function from running until all the assets have been loaded.
+ * This ensures that the assets are available for immediate use in setup() and draw().
+ */
+function preload() {
+    bg = loadImage("./assets/mountains.jpg");
+    birdImage = loadImage("./assets/bird.png");
+}
 
 function setup() {
     createCanvas(600, 600);
-    player = new Player(width / 2, height / 2, 25);
+    player = new Player(width / 2, height / 2, 25, birdImage);
 }
 
 function draw() {
-    background(0);
+    background(bg);
 
     player.update();
 }
--- 04_loading_assets/player.js
+++ 04_loading_assets/player.js
@@ -3,13 +3,14 @@ class Player {
     static GRAVITY = 0.4;
     static LIFT = -12;
 
-    constructor(x, y, radius){
+    constructor(x, y, radius, image){
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.diameter = this.radius * 2;
-
         this.yVelocity = 0;
+        this.image = image;
+
     }
 
     update() {
@@ -41,7 +42,30 @@ class Player {
     }
 
     draw() {
-        ellipse(this.x, this.y, this.diameter);
+        // ellipse(this.x, this.y, this.diameter);
+
+    /**
+     * With P5 JS, when you use an image, you are getting a containing rectangle
+     * to place the image inside of. The top-left corner of the image is placed at
+     * the coordinates (this.x, this.y). This means that the position (this.x, this.y)
+     * corresponds to the top-left corner of the rectangle that contains the image.
+     *
+     */
+        // image(this.image, this.x, this.y, this.diameter, this.diameter);
+
+
+        /**
+         * If you would like to center the image inside of the containing rectangle, you do this,
+         * in the case that you have a circular image, like we do.
+         * Notice that doing this allows us to not have to change the logic for the collision
+         * on the sides of the "bird". Doing this gives us the animation we want.
+         */
+        image(
+            this.image, 
+            this.x - this.radius, 
+            this.y - this.radius, 
+            this.diameter, 
+            this.diameter);
     }
 
     moveLeft() {
