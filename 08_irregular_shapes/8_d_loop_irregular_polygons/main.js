a8321baa73d3fc794769a301e865936b2ff5e252
Jacob McAllister <mcallisterj@westerntc.edu>
2024-07-15 10:50:33 -0500

    Adding notes
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- 08_irregular_shapes/8_d_loop_irregular_polygons/main.js
+++ 08_irregular_shapes/8_d_loop_irregular_polygons/main.js
@@ -11,6 +11,8 @@ function draw() {
   
   beginShape();
   for (let i = 0; i < totalVertices; i++) {
+    // TWO_PI is defined in P5 JS as the circumference of the
+    // circle in radians.
     let angle = map(i, 0, totalVertices, 0, TWO_PI);
     let x = 200 + radius * cos(angle);
     let y = 200 + radius * sin(angle);
